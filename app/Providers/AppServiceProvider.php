dab82592a5e6aeaf58396f6daa29facc05fe9ff0
alex <alex@alexsykes.net>
2025-02-07 16:41:20 +0000

    Initial commit

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Auth\Notifications\VerifyEmail;
+use Illuminate\Notifications\Messages\MailMessage;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -20,5 +22,12 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         //
+        VerifyEmail::toMailUsing(function (object $notifiable, string $url) {
+            return (new MailMessage)
+                ->subject('Accept terms and conditions')
+                ->line('Click the button below to confirm that you have had the opportunity to read the attached Terms and Conditions and Privacy Policy and accept the terms and conditions stated within these documents.')
+                ->attach('files/ASP_FS80.pdf')
+                ->action('Verify Email Address', $url);
+        });
     }
 }
