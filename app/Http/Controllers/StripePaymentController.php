244e33ee14dafd821a0de1afaa4d917ba5e4acaa
alex <alex@alexsykes.net>
2025-03-06 07:35:13 +0000

    start_06_03_25

---
+++ app/Http/Controllers/StripePaymentController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Config;
+
+class StripePaymentController extends Controller
+{
+
+    public function stripe()
+    {
+        $product = Config::get('stripe.product');
+        return view('stripe', compact('product'));
+    }
+
+    public function stripeCheckout(Request $request)
+    {
+        $stripe = new \Stripe\StripeClient(Config::get('stripe.stripe_secret_key'));
+
+        $redirectUrl = route('stripe.checkout.success') . '?session_id={CHECKOUT_SESSION_ID}';
+        $response =  $stripe->checkout->sessions->create([
+            'success_url' => $redirectUrl,
+            'line_items' => [
+                [
+                    'price' => 'price_1Qyfg1RmEmasrI2yJkwTqheV',
+                    'quantity' => 12,
+                ],
+            ],
+//            'phone_number_collection' => true,
+            'phone_number_collection' => ['enabled' => true],
+            'mode' => 'payment',
+            'allow_promotion_codes' => false
+        ]);
+
+        return redirect($response['url']);
+    }
+
+    public function stripeCheckoutSuccess(Request $request)
+    {
+        $stripe = new \Stripe\StripeClient(Config::get('stripe.stripe_secret_key'));
+
+        $session = $stripe->checkout->sessions->retrieve($request->session_id);
+        info($session);
+
+        $successMessage = "We have received your payment request and will let you know shortly.";
+
+        return view('success', compact('successMessage'));
+    }
+}
