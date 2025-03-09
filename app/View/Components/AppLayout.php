da5de12be5d6313a54dd9664865dabffb45b9e3b
alex <alex@alexsykes.net>
2025-03-09 07:09:38 +0000

    Start 09_03_25 repaired

--- app/View/Components/AppLayout.php
+++ app/View/Components/AppLayout.php
@@ -1,17 +1,17 @@
 <?php
 
-namespace bootstrap\View\Components;
+namespace App\View\Components;
 
 use Illuminate\View\Component;
 use Illuminate\View\View;
 
-class GuestLayout extends Component
+class AppLayout extends Component
 {
     /**
      * Get the view / contents that represents the component.
      */
     public function render(): View
     {
-        return view('layouts.guest');
+        return view('layouts.app');
     }
 }
