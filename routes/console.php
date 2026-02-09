1c782c728a92da5bf04156d1720e5c3a6d744f83
alex <alex@alexsykes.net>
2026-02-09 16:07:39 +0000

    Synced to LiveSite - 09_02_26

--- routes/console.php
+++ routes/console.php
@@ -7,8 +7,9 @@ use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
+    info("Running: inspire");
 })->purpose('Display an inspiring quote');
 
 // Check for unconfirmed entries
-Schedule::command(CheckEntries::class)->dailyAt('01:00');
+//Schedule::command(CheckEntries::class)->dailyAt('01:00');
 //Schedule::command( CheckEntries::class)->everyMinute();
