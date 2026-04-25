b11bad869ce4c9007463a83ac16c476b2dfe8de1
alex <alex@alexsykes.net>
2026-04-25 07:14:28 +0100

    WebContact start

--- app/Events/RefundCreated.php
+++ app/Events/RefundCreated.php
@@ -17,8 +17,8 @@ class RefundCreated
      */
     public function __construct($object, $metadata)
     {
-//        $this->object = $object;
-//        $this->metadata = $metadata;
+        $this->object = $object;
+        $this->metadata = $metadata;
     }
 
     /**
